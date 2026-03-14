5553c26a8708018b5990bc1472969a70faf32af1
minimaltek <cycle2012@gmail.com>
2026-03-14 12:20:13 +0900

    Initial Commit

---
+++ Magnetic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Magnetic
+//
+//  Created by minimaltek on 2026/03/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Magnetic/MagneticApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagneticApp.swift
+//  Magnetic
+//
+//  Created by minimaltek on 2026/03/14.
+//
+
+import SwiftUI
+
+@main
+struct MagneticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
